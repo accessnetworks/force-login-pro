151c3ad030593a5d5949a3a1b0f99723b16c3a49
Brandon Hubbard <bkhubbard@gmail.com>
2019-04-09 10:54:35 -0700

    scaffold

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Force_Login_Pro
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
